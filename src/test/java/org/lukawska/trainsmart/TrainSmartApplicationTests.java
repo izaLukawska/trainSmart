3364d96a160c06d165dd03d4271ef885e4da3457
Izabela Łukawska <168461455+izaLukawska@users.noreply.github.com>
2025-09-30 14:32:05 +0200

    init commit: project set up

---
+++ src/test/java/org/lukawska/trainsmart/TrainSmartApplicationTests.java
@@ -0,0 +1,12 @@
+package org.lukawska.trainsmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
