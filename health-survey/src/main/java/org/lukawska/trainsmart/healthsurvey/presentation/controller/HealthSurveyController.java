a1f2be35e1e05000cc67008b5086d2810595c879
Izabela Łukawska <168461455+izaLukawska@users.noreply.github.com>
2025-11-08 16:44:30 +0100

    changed path url in HealthSurveyController

--- health-survey/src/main/java/org/lukawska/trainsmart/healthsurvey/presentation/controller/HealthSurveyController.java
+++ health-survey/src/main/java/org/lukawska/trainsmart/healthsurvey/presentation/controller/HealthSurveyController.java
@@ -14,38 +14,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/users/health-survey")
+@RequestMapping("/api/users/{userId}/health-survey")
 @RequiredArgsConstructor
 @Validated
 public class HealthSurveyController {
 
     private final HealthSurveyService healthSurveyService;
 
-    @GetMapping("/{userId}")
+    @GetMapping
     public HealthSurveyResponse getHealthSurveyByUserId(@PathVariable @Positive Long userId) {
         return healthSurveyService.getHealthSurveyByUserIdResponse(userId);
     }
 
-    @GetMapping("/{userId}/injuries")
+    @GetMapping("/injuries")
     public List<String> getAllInjuriesByUserId(@PathVariable @Positive Long userId) {
         return healthSurveyService.getAllInjuriesByUserId(userId);
     }
 
-    @PostMapping("/submit")
+    @PostMapping
     public ResponseEntity<HealthSurveyResponse> submitHealthSurvey(@Valid
                                                                    @RequestBody
                                                                    CreateHealthSurveyRequest surveyRequest) {
         return ResponseEntity.status(201).body(healthSurveyService.submitHealthSurvey(surveyRequest));
     }
 
-    @PutMapping("/update")
+    @PutMapping
     public ResponseEntity<HealthSurveyResponse> updateHealthSurvey(@Valid
                                                                    @RequestBody
                                                                    UpdateHealthSurveyRequest surveyRequest) {
         return ResponseEntity.ok(healthSurveyService.updateHealthSurvey(surveyRequest));
     }
 
-    @DeleteMapping("/delete/{userId}")
+    @DeleteMapping
     public ResponseEntity<Void> deleteHealthSurvey(@PathVariable @Positive Long userId) {
         healthSurveyService.deleteHealthSurveyByUserId(userId);
         return ResponseEntity.noContent().build();
