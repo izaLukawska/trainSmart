3c8fc09f8abb761caadcd9c80310350d934ef3e6
Izabela Łukawska <168461455+izaLukawska@users.noreply.github.com>
2025-11-11 15:28:39 +0100

    added logger in user service

--- shared-persistence/src/main/java/org/lukawska/trainsmart/shared_persistence/application/service/UserService.java
+++ shared-persistence/src/main/java/org/lukawska/trainsmart/shared_persistence/application/service/UserService.java
@@ -1,6 +1,7 @@
 package org.lukawska.trainsmart.shared_persistence.application.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.lukawska.trainsmart.shared_persistence.application.exception.UserNotFoundException;
 import org.lukawska.trainsmart.shared_persistence.domain.entities.User;
 import org.lukawska.trainsmart.shared_persistence.domain.repositories.UserRepository;
@@ -8,12 +9,14 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
 
     public User getUserById(Long userId) {
-        return userRepository.findById(userId)
-                             .orElseThrow(() -> new UserNotFoundException(userId));
+        log.info("Retrieving user for ID: {}", userId);
+
+        return userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
     }
 }
