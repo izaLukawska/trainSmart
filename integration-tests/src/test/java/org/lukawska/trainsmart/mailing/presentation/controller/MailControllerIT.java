032a37eaab275286fc908012faa4bd0539b40162
Izabela Łukawska <168461455+izaLukawska@users.noreply.github.com>
2025-11-14 16:06:38 +0100

    integration tests for mailing module

---
+++ integration-tests/src/test/java/org/lukawska/trainsmart/mailing/presentation/controller/MailControllerIT.java
@@ -0,0 +1,106 @@
+package org.lukawska.trainsmart.mailing.presentation.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.lukawska.trainsmart.mailing.application.dto.MailRequest;
+import org.lukawska.trainsmart.mailing.application.dto.MailResponse;
+import org.lukawska.trainsmart.mailing.application.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.lukawska.trainsmart.mailing.testutil.MailingTestData.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MailController.class)
+@ActiveProfiles("test")
+public class MailControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private MailService mailService;
+
+    @Test
+    void shouldCreateMailAndReturn201() throws Exception {
+        // given
+        final MailRequest mailRequest = mailRequestWithAttachments();
+        when(mailService.sendMail(mailRequest)).thenReturn(mailResponseWithId(1L));
+
+        // when + then
+        mockMvc.perform(post("/api/mail/send").contentType(MediaType.APPLICATION_JSON)
+                                              .content(objectMapper.writeValueAsString(mailRequest)))
+               .andExpect(status().isCreated());
+    }
+
+    @Test
+    void shouldReturnMailById() throws Exception {
+        // given
+        final MailResponse mailResponse = mailResponseWithId(1L);
+        when(mailService.getMailResponseById(1L)).thenReturn(mailResponse);
+
+        // when + then
+        mockMvc.perform(get("/api/mail/1"))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.id").value(mailResponse.id()))
+               .andExpect(jsonPath("$.subject").value(mailResponse.subject()))
+               .andExpect(jsonPath("$.recipients[0]").value(mailResponse.recipients().getFirst()));
+    }
+
+    @Test
+    void shouldReturnAllMailsByRecipient() throws Exception {
+        // given
+        final String recipient = "recipient@test.com";
+        when(mailService.getAllMailsByRecipient(recipient))
+                .thenReturn(List.of(mailResponseWithRecipient(recipient), mailResponseWithRecipient(recipient)));
+
+        // when + then
+        mockMvc.perform(get("/api/mail/recipient").param("recipient", recipient))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.length()").value(2))
+               .andExpect(jsonPath("$[*].recipients", everyItem(hasItem(recipient))));
+    }
+
+    @Test
+    void shouldReturnAllMailsBySubject() throws Exception {
+        // given
+        final String subject = "subject";
+        final String keyword = "sub";
+        when(mailService.getAllMailsBySubjectContaining(keyword))
+                .thenReturn(List.of(mailResponseWithSubject(subject), mailResponseWithSubject(subject)));
+
+        // when + then
+        mockMvc.perform(get("/api/mail/subject").param("keyword", keyword))
+               .andExpect(status().isOk())
+               .andExpect(jsonPath("$.length()").value(2))
+               .andExpect(jsonPath("$[*].subject", everyItem(containsString(keyword))));
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenInvalidPathVariable() throws Exception {
+        //when && then
+        mockMvc.perform(get("/api/mail/-1")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void shouldReturnBadRequestWhenInvalidRequestBody() throws Exception {
+        //when && then
+        mockMvc.perform(post("/api/mail/send").contentType(MediaType.APPLICATION_JSON)
+                                              .content("{}"))
+               .andExpect(status().isBadRequest());
+    }
+}
