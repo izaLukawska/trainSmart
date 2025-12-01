387695d703d2e8a82dfb9fdb6bccd7efefbaa302
Izabela Łukawska <168461455+izaLukawska@users.noreply.github.com>
2025-12-01 12:17:11 +0100

    code refactor

--- training-plan/src/main/java/org/lukawska/trainsmart/trainingplan/application/mapper/TrainingPlanMapper.java
+++ training-plan/src/main/java/org/lukawska/trainsmart/trainingplan/application/mapper/TrainingPlanMapper.java
@@ -9,6 +9,6 @@ import org.lukawska.trainsmart.trainingplan.domain.entity.TrainingPlan;
 public class TrainingPlanMapper {
 
     public static TrainingPlan mapToTrainingPlan(User user, TrainingPlanRequest request) {
-        return new TrainingPlan(user, request.trainingType(), request.planDuration(), request.daysPerWeek());
+        return new TrainingPlan(user, request.trainingType(), request.planDuration(), request.preferredDays().size());
     }
 }
