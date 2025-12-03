ee20f2374e6a42b1db4d68dd6c0327034d3b527c
Izabela Łukawska <168461455+izaLukawska@users.noreply.github.com>
2025-12-03 17:17:06 +0100

    sharedpersistance: added annotation on UserRepository and code refactor

--- shared-persistence/src/main/java/org/lukawska/trainsmart/sharedpersistence/domain/repositories/UserRepository.java
+++ shared-persistence/src/main/java/org/lukawska/trainsmart/sharedpersistence/domain/repositories/UserRepository.java
@@ -2,5 +2,7 @@ package org.lukawska.trainsmart.sharedpersistence.domain.repositories;
 
 import org.lukawska.trainsmart.sharedpersistence.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {}
