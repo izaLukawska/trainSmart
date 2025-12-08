06155489cd8ce8cef19091530e3576fcba41e700
Izabela Łukawska <168461455+izaLukawska@users.noreply.github.com>
2025-12-08 20:00:25 +0100

    added audit security, removed redundant prefix in MailingController

--- app/src/main/java/org/lukawska/trainsmart/logging/CorrelationIdFilter.java
+++ app/src/main/java/org/lukawska/trainsmart/logging/CorrelationIdFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.MDC;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -20,9 +21,9 @@ public class CorrelationIdFilter extends OncePerRequestFilter {
     private static final String MDC_KEY = "correlationId";
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         String correlationId = request.getHeader(HEADER);
 
         if (StringUtils.isBlank(correlationId)) {
--- app/src/main/java/org/lukawska/trainsmart/persistence/AuditorAwareImpl.java
+++ app/src/main/java/org/lukawska/trainsmart/persistence/AuditorAwareImpl.java
@@ -1,6 +1,10 @@
 package org.lukawska.trainsmart.persistence;
 
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -8,8 +12,11 @@ import java.util.Optional;
 @Component
 public class AuditorAwareImpl implements AuditorAware<String> {
 
+    @NonNull
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("anonymous");
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                       .filter(auth -> auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken))
+                       .map(Authentication::getName);
     }
 }
