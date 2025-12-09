33189435ec25cb8471c16565395d92ea426b7c50
Izabela Łukawska <168461455+izaLukawska@users.noreply.github.com>
2025-12-09 19:32:25 +0100

    added mocked bean in MailingRepositoryIT for a project to build successfully

--- integration-tests/src/test/java/org/lukawska/trainsmart/mailing/domain/repository/MailingRepositoryIT.java
+++ integration-tests/src/test/java/org/lukawska/trainsmart/mailing/domain/repository/MailingRepositoryIT.java
@@ -2,6 +2,7 @@ package org.lukawska.trainsmart.mailing.domain.repository;
 
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.lukawska.trainsmart.commons.jwt.JwtService;
 import org.lukawska.trainsmart.config.PostgresTestBase;
 import org.lukawska.trainsmart.mailing.domain.entities.MailEntity;
 import org.lukawska.trainsmart.mailing.testutil.MailingTestData;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import java.util.List;
 
@@ -20,6 +22,9 @@ import static org.lukawska.trainsmart.mailing.testutil.MailingTestData.mailWithR
 @Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MailingRepositoryIT extends PostgresTestBase {
+
+    @MockitoBean
+    private JwtService jwtService;
 
     @Autowired
     private MailRepository mailRepository;
